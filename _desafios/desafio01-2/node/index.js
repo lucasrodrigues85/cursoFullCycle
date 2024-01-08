fb4f4c4046c56ba5bbd270bfab3aa2b44c640bc2
Lucas M. Rodrigues <lucas@scienceweb.com.br>
2024-01-08 12:43:27 -0300

    chore: correções

--- _desafios/desafio01-2/node/index.js
+++ _desafios/desafio01-2/node/index.js
@@ -9,7 +9,7 @@ const config = {
     database: 'nodedb'
 }
 
-const mysql = require('mysql2')
+const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
 app.get('/', (req, res) => {
